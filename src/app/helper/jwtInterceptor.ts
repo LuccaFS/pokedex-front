bab6eefac2ecab3af34365c7419f2634417c3bc1
LuccaFS <luccafde@examroom.ai>
2022-11-23 16:37:03 -0300

    Removal of comments.

--- src/app/helper/jwtInterceptor.ts
+++ src/app/helper/jwtInterceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {  HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { delay, Observable, switchMap, from as observableFrom } from 'rxjs';
+import { delay, Observable, switchMap, from  } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import * as fromAuth from '../state/auth/auth.reducer';
@@ -14,13 +14,7 @@ export class JwtInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     // add auth header with jwt if account is logged in and request is to the api url
-    //let token = '';
-
-
-
-
-
-    return observableFrom(this._store.select(fromAuth.selectToken)
+    return from(this._store.select(fromAuth.selectToken)
     .pipe(
       switchMap(token => {
         const isApiUrl = request.url.startsWith('https://localhost:5001/api/Access/');
@@ -32,25 +26,5 @@ export class JwtInterceptor implements HttpInterceptor {
         return next.handle(request);
       })
     ));
-
-
-
-
-
-
-
-
-/*
-
-    const isLoggedIn = token != '';
-    const isApiUrl = request.url.startsWith('https://localhost:5001/api/Access/');
-    console.log('Adicionando token')
-    if (isLoggedIn && isApiUrl) {
-      request = request.clone({
-        setHeaders: { Authorization: `Bearer ${token}` },
-      });
-    }
-
-    return next.handle(request);*/
   }
 }
