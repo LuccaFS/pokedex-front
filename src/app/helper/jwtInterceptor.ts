de85ed805a72fff8dbaf5fb8d39ee63595ce1b0f
LuccaFS <luccafde@examroom.ai>
2022-11-23 16:34:11 -0300

    jwtInterceptor.ts update for time delays

--- src/app/helper/jwtInterceptor.ts
+++ src/app/helper/jwtInterceptor.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {  HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { delay, Observable, switchMap, from as observableFrom } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import * as fromAuth from '../state/auth/auth.reducer';
@@ -19,17 +14,43 @@ export class JwtInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     // add auth header with jwt if account is logged in and request is to the api url
-    let token = '';
-    this._store.select(fromAuth.selectToken)
-      .subscribe((value: any) => (token = value));
+    //let token = '';
+
+
+
+
+
+    return observableFrom(this._store.select(fromAuth.selectToken)
+    .pipe(
+      switchMap(token => {
+        const isApiUrl = request.url.startsWith('https://localhost:5001/api/Access/');
+        if (token && isApiUrl) {
+          request = request.clone({
+            setHeaders: { Authorization: `Bearer ${token}` },
+          });
+        }
+        return next.handle(request);
+      })
+    ));
+
+
+
+
+
+
+
+
+/*
+
     const isLoggedIn = token != '';
     const isApiUrl = request.url.startsWith('https://localhost:5001/api/Access/');
+    console.log('Adicionando token')
     if (isLoggedIn && isApiUrl) {
       request = request.clone({
         setHeaders: { Authorization: `Bearer ${token}` },
       });
     }
 
-    return next.handle(request);
+    return next.handle(request);*/
   }
 }
