915e13745bdc28569c0d716c9789677c1dab46f6
LuccaFS <luccafde@examroom.ai>
2023-01-23 17:18:45 -0300

    Gen 9 update special filter

--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -36,12 +36,56 @@ export class PokemonComponent implements OnInit {
 
   }
 
-  filterSearch(searchValue: any){
+  filterSearch(searchText: any){
+    const searchValue = searchText.toLowerCase();
     this.PokemonList = this.Pokemons.filter((item: Pokemon) => {
-      if(searchValue=="ub"){
+      //Special cases
+      if(searchValue=="legends" || searchValue=="legendary"){ //Legendary
+        return item.isLegendary;
+
+      }else if(searchValue=="ub" || searchValue=="ultra beasts"){ //Ultra Beasts
         return item.idPokemon>792 && item.idPokemon<807;
-      }else{
-        return item.dsName.toLowerCase().includes(searchValue.toLowerCase()) || item.idPokemon == +searchValue;
+
+      }else if(searchValue=="paradox"){ //All Paradox
+        return item.idPokemon>=984 && item.idPokemon<=995 || item.idPokemon>=1005 && item.idPokemon<=1006;
+
+      }else if(searchValue=="paradox past" || searchValue=="past" || searchValue=="past paradox"){ //Past Paradox
+        return item.idPokemon>=984 && item.idPokemon<990 || item.idPokemon==1005;
+
+      }else if(searchValue=="paradox future"|| searchValue=="future" || searchValue=="future paradox"){ //Future Paradox
+        return item.idPokemon>989 && item.idPokemon<=995 || item.idPokemon==1006;
+
+      //Specific paradox cases
+      }else if(searchValue=="donphan paradox"){
+        return item.idPokemon==232 || item.idPokemon==984 || item.idPokemon==990;
+      }else if(searchValue=="volcarona paradox"){
+        return item.idPokemon==637 || item.idPokemon==988 || item.idPokemon==994;
+
+      }else if(searchValue=="jigglypuff paradox"){
+        return item.idPokemon==39 || item.idPokemon==985;
+      }else if(searchValue=="amoonguss paradox"){
+        return item.idPokemon==591 || item.idPokemon==986;
+      }else if(searchValue=="misdreavus paradox"){
+        return item.idPokemon==200 || item.idPokemon==987;
+      }else if(searchValue=="magneton paradox"){
+        return item.idPokemon==82 || item.idPokemon==989;
+      }else if(searchValue=="salamence paradox"){
+        return item.idPokemon==373 || item.idPokemon==1005;
+
+      }else if(searchValue=="delibird paradox"){
+        return item.idPokemon==225 || item.idPokemon==991;
+      }else if(searchValue=="hariyama paradox"){
+        return item.idPokemon==297 || item.idPokemon==992;
+      }else if(searchValue=="hydreigon paradox"){
+        return item.idPokemon==635 || item.idPokemon==993;
+      }else if(searchValue=="tyranitar paradox"){
+        return item.idPokemon==248 || item.idPokemon==995;
+      }else if(searchValue=="gardevoir paradox" || searchValue=="gallade paradox"){
+        return item.idPokemon==282 || item.idPokemon==475 || item.idPokemon==1006;
+
+
+      }else{ //normal search
+        return item.dsName.toLowerCase().includes(searchValue) || item.idPokemon == +searchText;
       }
     });
   }
